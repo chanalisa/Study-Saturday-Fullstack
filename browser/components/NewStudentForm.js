7e66c3c2cd096c17cd0728270a15790fdc4dc036
Alisa Chan <alisachan.ny@gmail.com>
2019-10-12 13:53:17 -0400

    completed change handler and attempted submit handler

--- browser/components/NewStudentForm.js
+++ browser/components/NewStudentForm.js
@@ -1,22 +1,60 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class NewStudentForm extends Component {
+    constructor () {
+        super ();
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const field = event.target.name;
+        this.setState({
+            [field]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(event.target)
+        const newStudent = {
+            firstName: event.target.firstName,
+            lastName: event.target.lastName,
+            email: event.target.email
+        }
+        async axios.post('/', {newStudent}) {
+            await Student.create(res.data)
+        }
+        
+        this.setState({
+            firstName: '',
+            lastName: '',
+            email: ''
+        })
+    }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
           First Name:
-          <input type="text" name="firstName" />
+          <input type="text" name="firstName" value={this.state.value} onChange={this.handleChange} />
         </label>
 
         <label>
           Last Name:
-          <input type="text" name="lastName" />
+          <input type="text" name="lastName" onChange={this.handleChange} />
         </label>
 
         <label>
           Email:
-          <input type="email" name="email" />
+          <input type="email" name="email" onChange={this.handleChange} />
         </label>
 
         <button type="submit">Submit New Student</button>
